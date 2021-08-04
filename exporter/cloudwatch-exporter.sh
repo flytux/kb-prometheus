54ff6046840e38d8ab2f9956934bae2d5daf4a12
flytux <hooney.jung@gmail.com>
2021-08-04 08:57:32 +0000

    cloudwatch update

--- exporter/cloudwatch-exporter.sh
+++ exporter/cloudwatch-exporter.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker run -p 9106:9106 --rm --name cloudwatch \
+docker run -p 9106:9106 --rm --name cloudwatch -d \
            -v `pwd`/config/cloudwatch-config.yml:/config/cloudwatch-config.yml \
            -e AWS_ACCESS_KEY_ID=AKIAVZLPMYUGZWGORS6Q \
            -e AWS_SECRET_ACCESS_KEY=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 \
--- prometheus/prom.sh
+++ prometheus/prom.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-export PROM_HOME=/home/kwadmin/works/kb-prometheus/prometheus
+export PROM_HOME=$PWD
 
-docker run --rm --name prom -d -p 9090:9090 \
+docker run --name prom -d -p 9090:9090 \
   -v $PROM_HOME/config:/etc/prometheus \
   prom/prometheus:v2.28.1
 
